504b9e2973f530854cac4f3daf044744551b93a4
fundon <cfddream@gmail.com>
2014-10-28 23:22:38 +0800

    update

--- README.md
+++ README.md
@@ -15,3 +15,15 @@ import . "github.com/go-libs/methods"
 quest.Request(GET, "http://httpbin.org/get")
 ```
 
+
+### Response Handling
+
+```go
+quest.Request(GET, "http://httpbin.org/get").
+  Response(func(request *http.Request, response *http.Response, data interface{}, err error) {
+  fmt.Println(request)
+  fmt.Println(response)
+  fmt.Println(data)
+  fmt.Println(err)
+})
+```
--- quest_test.go
+++ quest_test.go
@@ -1,6 +1,8 @@
 package quest
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	. "github.com/go-libs/methods"
@@ -13,5 +15,15 @@ func TestMakeARequest(t *testing.T) {
 
 	mocha.Convey("Should be making a Request", t, func() {
 		mocha.So(q.Method, mocha.ShouldEqual, GET)
+	})
+}
+
+func TestResponseHandling(t *testing.T) {
+	Request(GET, "http://httpbin.org/get").
+		Response(func(request *http.Request, response *http.Response, data interface{}, err error) {
+		fmt.Println(request)
+		fmt.Println(response)
+		fmt.Println(data)
+		fmt.Println(err)
 	})
 }
