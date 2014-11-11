085e1847776670a75902d3d5cd17e699fefa6c0c
fundon <cfddream@gmail.com>
2014-11-12 01:53:18 +0800

    processbar

---
+++ processbar.go
@@ -0,0 +1,17 @@
+package quest
+
+type ProgressBar struct {
+	Qreq     *Qrequest
+	Total    int64
+	Current  int64
+	Progress func(current, total, expected int64)
+}
+
+func (p *ProgressBar) Write(b []byte) (int, error) {
+	current := len(b)
+	if p.Total != -1 {
+		p.Current += int64(current)
+		p.Progress(p.Current, p.Total, p.Total-p.Current)
+	}
+	return current, nil
+}
