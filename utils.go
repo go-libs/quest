237e2f1ef871408d0d2acc61b94dc3f4f6c6b599
fundon <cfddream@gmail.com>
2014-11-16 22:43:24 +0800

    add utils.go

---
+++ utils.go
@@ -0,0 +1,53 @@
+package quest
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"strings"
+)
+
+func nopCloser(r io.Reader) io.ReadCloser {
+	return ioutil.NopCloser(r)
+}
+
+func packBodyByString(s string) (io.ReadCloser, int64) {
+	return nopCloser(bytes.NewBufferString(s)), int64(len(s))
+}
+
+func packBodyByBytes(b []byte) (io.ReadCloser, int64) {
+	return nopCloser(bytes.NewBuffer(b)), int64(len(b))
+}
+
+func packBodyByBytesBuffer(b *bytes.Buffer) (io.ReadCloser, int64) {
+	return nopCloser(b), int64(b.Len())
+}
+
+func packBodyByBytesReader(b *bytes.Reader) (io.ReadCloser, int64) {
+	return nopCloser(b), int64(b.Len())
+}
+
+func packBodyByPipeReader(pr *io.PipeReader) (io.ReadCloser, int64) {
+	b := new(bytes.Buffer)
+	length, _ := b.ReadFrom(pr)
+	return nopCloser(b), length
+}
+
+func packBodyByReader(pr io.Reader) (io.ReadCloser, int64) {
+	b := new(bytes.Buffer)
+	length, _ := b.ReadFrom(pr)
+	return nopCloser(b), length
+}
+
+func packBodyByStringsReader(b *strings.Reader) (io.ReadCloser, int64) {
+	return nopCloser(b), int64(b.Len())
+}
+
+func encodesParametersInURL(method Method) bool {
+	switch method {
+	case GET, HEAD, DELETE:
+		return true
+	default:
+		return false
+	}
+}
